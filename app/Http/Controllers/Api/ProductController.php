691154df332408f42f7ee8b51edc63b240b907e8
skilfulsidiq2 <obalowu.sodiq@707digital.com>
2021-09-16 11:48:43 +0100

    er

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Api\BaseController;
+use Illuminate\Http\Request;
+
+class ProductController extends BaseCon
+
+{
+    /**
+     * Admin funcitonalities
+     */
+    //all products
+    public function adminProductList(){}
+
+    //Admin add or update product
+    public function addOrUpdateProduct(Request $request,$slug=null){}
+    //delete a product using
+    public function deleteProduct($slug){}
+
+    /**All users */
+    public function productList(){}
+}
